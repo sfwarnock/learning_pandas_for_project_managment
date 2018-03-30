dc4d6b3b245509d67b2e327c0c4d63eef23eab56
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-03-30 15:04:55 -0600

    Add files via upload

--- read_csv.py
+++ read_csv.py
@@ -8,5 +8,10 @@ Created on Tue Mar 27 13:33:46 2018
 import pandas as pd
 
 # Read file.
-
 data_file = pd.read_csv('datafile.csv')
+
+# Sum one coloumn.
+df1 = data_file['Mar-18'].sum()
+
+# Sum one coloumn with new total row of summed column.
+data_file.loc['Total'] = pd.Series(data_file['Total Cost'].sum(), index = ['Total Cost']);print(data_file)
