ffe2d6fbefede3b752d3407add8f860f0971c2a5
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-04-09 12:31:45 -0600

    Add files via upload

--- read_csv.py
+++ read_csv.py
@@ -8,10 +8,20 @@ Created on Tue Mar 27 13:33:46 2018
 import pandas as pd
 
 # Read file.
-data_file = pd.read_csv('datafile.csv')
+data_file = pd.read_csv('datafile.csv').fillna(0)
 
 # Sum one coloumn.
-df1 = data_file['Mar-18'].sum()
+#df1 = data_file['Mar-18'].sum()
 
 # Sum one coloumn with new total row of summed column.
-data_file.loc['Total'] = pd.Series(data_file['Total Cost'].sum(), index = ['Total Cost']);print(data_file)
+#data_file.loc['Total'] = pd.Series(data_file['Total Cost'].sum(), index = ['Total Cost']);print(data_file)
+
+# Total timephased data rows. Sum all rows with months.
+data_file['Total Sum'] = data_file['Mar-18'] + data_file['Apr-18'] + data_file['May-18'] + data_file['Jun-18'] + data_file['Jul-18'] + data_file['Aug-18']
+
+#df['Total'] = df['Mar-18'] + df['Apr-18']
+
+#[0:15], 'May-18':[0:15],'Jun-18':[0:15], 'Jul-18':[0:15], 'Aug-18':[0:15]})
+
+#   Sum all timephases columns.
+#data_file.loc['Total'] = pd.Series(data_file['Total Cost'].sum(), index = ['Total Cost']);print(data_file)
