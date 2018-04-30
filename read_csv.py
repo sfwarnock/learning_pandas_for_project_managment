095452acbbea079c05d4673a941bc8bb4a9cc0b2
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-04-30 15:44:05 -0600

    Add files via upload

--- read_csv.py
+++ read_csv.py
@@ -7,6 +7,8 @@ Created on Tue Mar 27 13:33:46 2018
 
 import pandas as pd
 
+#import matplotlib.pyplot as plt
+
 # Read file.
 data_file = pd.read_csv('datafile.csv').fillna(0)
 
@@ -15,25 +17,25 @@ data_file['Total Sum'] = data_file['Mar-18'] + data_file['Apr-18'] + data_file['
 
 # Sum all coloumns with budgeted ammounts.
 data_file.loc['Total TPD'] = pd.Series(data_file[['Total Cost', 'Mar-18','Apr-18','May-18','Jun-18','Jul-18','Aug-18','Total Sum']].sum(), 
-             index = ['Total Cost', 'Mar-18','Apr-18','May-18','Jun-18','Jul-18','Aug-18','Total Sum'])
+             index = ['Total Cost', 'Mar-18','Apr-18','May-18','Jun-18','Jul-18','Aug-18','Total Sum']);print(data_file)
 
 # Make cumalative dataseires for S graphs.
-data_file.loc['Total Cumulative'] = pd.Series(data_file[['Mar-18']].sum(), 
-             index = ['Mar-18']);print(data_file)
+#data_file.loc['Total Cumulative'] = pd.Series(data_file[['Mar-18']].sum(), index = ['Mar-18']);print(data_file)
 
 # Make a list of all Cams.
 
-def cams():
-    name = data_file.CAM.dropna().tolist()
-    cam = []
+name = data_file.CAM.dropna().tolist()
+cam = []
 
-    for name in name:
-        if name not in cam:
-            cam.append(name)
-    print(cam)     
+for name in name:
+    if name not in cam:
+        cam.append(name)
+        
+#Generate new dataframe for each cam.
+newDF0 = data_file[data_file.CAM == cam[0]]
+newDF1 = data_file[data_file.CAM == cam[1]]
+newDF2 = data_file[data_file.CAM == cam[2]]
+newDF3 = data_file[data_file.CAM == cam[3]]
 
-cams()
 
-# Generate datatables for each cam.
-
-# Graph dataset for each cam.
+print(cam)
