e93aa55f364c642051afba280fe46da74ce988c2
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-06-01 10:03:20 -0600

    Add files via upload

---
+++ s-curve.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  1 08:19:04 2018
+
+@author: Scott Warnock
+"""
+
+# s-curve.py
+
+import pandas as pd
+#import matplotlib.pyplot as plt
+
+
+# Read file.
+data_file = pd.read_csv('cumalative_data1.csv').fillna(0)
+
+data_file.plot.line(x = 0, y = ['BCWS','BCWP','ACWP'])
