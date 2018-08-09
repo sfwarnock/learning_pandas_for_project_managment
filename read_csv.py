e6bfe9f12935dc2da520d0555de7cb48aa2fed00
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-08-09 09:37:50 -0600

    Add files via upload

--- read_csv.py
+++ read_csv.py
@@ -19,29 +19,29 @@ data_file['Total Sum'] = data_file['Mar-18'] + data_file['Apr-18'] + data_file['
 data_file.loc['Total TPD'] = pd.Series(data_file[['Total Cost', 'Mar-18','Apr-18','May-18','Jun-18','Jul-18','Aug-18','Total Sum']].sum(), 
              index = ['Total Cost', 'Mar-18','Apr-18','May-18','Jun-18','Jul-18','Aug-18','Total Sum']);print(data_file)
 
+# Pviot table datatable by CAM and charge code.
+table = data_file.pivot_table(index=['CAM','Charge Code'])
+print(table)
+
 # Make cumalative dataseires for S graphs.
 #data_file.loc['Total Cumulative'] = pd.Series(data_file[['Mar-18']].sum(), index = ['Mar-18']);print(data_file)
 
 # Make a list of all Cams.
 
-name = data_file.CAM.dropna().tolist()
-cam = []
+#name = data_file.CAM.dropna().tolist()
+#cam = []
 
-for name in name:
-    if name not in cam:
-        cam.append(name)
-print(cam)
-        
+#for name in name:
+    #if name not in cam:
+        #cam.append(name)
+#print(cam)
+
 #Generate new dataframe for each cam.
-x = 0
-camDf = cam[0 + x]
 
-while True:
-    if camDf != cam[-1]:
-        camDf = data_file[data_file.CAM == camDf]
-        x += 1
-    else:
-        break
+#cam0 = data_file[data_file.CAM == cam[0]]
+#cam1 = data_file[data_file.CAM == cam[1]]
+#cam2 = data_file[data_file.CAM == cam[2]]
+#cam3 = data_file[data_file.CAM == cam[3]]
 
 #y = data_file[data_file.CAM == cam[x + 1]]
 #newDF2 = data_file[data_file.CAM == cam[0 + x]]
