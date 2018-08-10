f1348a155c6eaddace62acc9e095ba6f9a7df361
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-08-10 12:45:03 -0600

    Add files via upload

--- read_csv.py
+++ read_csv.py
@@ -6,43 +6,21 @@ Created on Tue Mar 27 13:33:46 2018
 """
 
 import pandas as pd
+#import numpy as np
 
 #import matplotlib.pyplot as plt
 
-# Read file.
+# read data file.
 data_file = pd.read_csv('datafile.csv').fillna(0)
-
-# Total timephased data rows. Sum all rows with months.
-data_file['Total Sum'] = data_file['Mar-18'] + data_file['Apr-18'] + data_file['May-18'] + data_file['Jun-18'] + data_file['Jul-18'] + data_file['Aug-18']
-
-# Sum all coloumns with budgeted ammounts.
-data_file.loc['Total TPD'] = pd.Series(data_file[['Total Cost', 'Mar-18','Apr-18','May-18','Jun-18','Jul-18','Aug-18','Total Sum']].sum(), 
-             index = ['Total Cost', 'Mar-18','Apr-18','May-18','Jun-18','Jul-18','Aug-18','Total Sum']);print(data_file)
+    
+# make list from header for pivot_table values.
+headerValues = data_file.columns.values[6:].tolist()
 
 # Pviot table datatable by CAM and charge code.
-table = data_file.pivot_table(index=['CAM','Charge Code'])
-print(table)
+table = pd.pivot_table(data_file, values = headerValues, index=['CAM','Charge Code','Value Type'])
+print (table)
 
-# Make cumalative dataseires for S graphs.
-#data_file.loc['Total Cumulative'] = pd.Series(data_file[['Mar-18']].sum(), index = ['Mar-18']);print(data_file)
+table['Grand Total'] = table.sum(axis = 1)
 
-# Make a list of all Cams.
-
-#name = data_file.CAM.dropna().tolist()
-#cam = []
-
-#for name in name:
-    #if name not in cam:
-        #cam.append(name)
-#print(cam)
-
-#Generate new dataframe for each cam.
-
-#cam0 = data_file[data_file.CAM == cam[0]]
-#cam1 = data_file[data_file.CAM == cam[1]]
-#cam2 = data_file[data_file.CAM == cam[2]]
-#cam3 = data_file[data_file.CAM == cam[3]]
-
-#y = data_file[data_file.CAM == cam[x + 1]]
-#newDF2 = data_file[data_file.CAM == cam[0 + x]]
-#newDF3 = data_file[data_file.CAM == cam[0 + x]]
+camTotals = table.sum(level = ['CAM','Value Type'])
+print(camTotals)
