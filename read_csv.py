fe29e27b5e85a0bb2e69e3129414d7bf5053a0c3
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-08-16 06:12:15 -0600

    Add files via upload

--- read_csv.py
+++ read_csv.py
@@ -12,15 +12,26 @@ import pandas as pd
 
 # read data file.
 data_file = pd.read_csv('datafile.csv').fillna(0)
-    
+
+acwp = data_file.loc[data_file['Value Type'] == 'ACWP']
+bcwp = data_file.loc[data_file['Value Type'] == 'BCWP']
+bcws = data_file.loc[data_file['Value Type'] == 'BCWS']
+
+# make list of CAMs for data processing
+name = data_file.CAM.dropna().tolist()
+cam = []
+for name in name:
+    if name not in cam:
+        cam.append(name)
+print(cam)
+
 # make list from header for pivot_table values.
 headerValues = data_file.columns.values[6:].tolist()
 
 # Pviot table datatable by CAM and charge code.
 table = pd.pivot_table(data_file, values = headerValues, index=['CAM','Charge Code','Value Type'])
-print (table)
 
-table['Grand Total'] = table.sum(axis = 1)
+table['Grand Total'] = table.sum(axis = 1); print(table)
 
 camTotals = table.sum(level = ['CAM','Value Type'])
 print(camTotals)
