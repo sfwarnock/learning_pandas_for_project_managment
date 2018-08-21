ee5c3c3ca5189cde6a515ad43dc3c1775145642d
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-08-21 06:23:10 -0600

    Add files via upload

--- read_csv.py
+++ read_csv.py
@@ -18,13 +18,13 @@ bcwp = data_file.loc[data_file['Value Type'] == 'BCWP']
 bcws = data_file.loc[data_file['Value Type'] == 'BCWS']
 
 # make list of CAMs for data processing
-name = data_file.CAM.dropna().tolist()
-cam = []
-for name in name:
-    if name not in cam:
-        cam.append(name)
-print(cam)
-
+cam_group = data_file.groupby('CAM')
+i = 0
+for cam, group in cam_group:
+    i = i + 1
+    print('Cam', i, cam)
+    print(group)
+    
 # make list from header for pivot_table values.
 headerValues = data_file.columns.values[6:].tolist()
 
