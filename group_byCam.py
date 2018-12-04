0dd2fe386143995def8e4ddcf6924d36957c3c4e
Scott Warnock <35125089+sfwarnock@users.noreply.github.com>
2018-12-04 07:34:12 -0700

    Add files via upload

--- group_byCam.py
+++ group_byCam.py
@@ -48,105 +48,23 @@ for columnHeaders in bcws_header:
 
 period_DataFrame = pd.concat([acwp, bcwp, bcws], sort = True).sort_values(by='CAM').reindex(columns = csv_header).fillna(0)
 
-# fiter by chargecode
-filter_ChargeCode = pd.pivot_table(period_DataFrame, values = csv_header, index=['Charge Code', 'CAM', 'Value Type'])
-print(filter_ChargeCode)
-
-# filter by CAM
-filter_CAM = pd.pivot_table(period_DataFrame, values = csv_header, index=['CAM', 'Charge Code', 'Value Type'])
-print(filter_CAM)
 
 period_BCWP = period_DataFrame.loc['Period Total Earned', headerValues]
 period_BCWS = period_DataFrame.loc['Period Total Planned', headerValues]
 period_ACWP = period_DataFrame.loc['Period Total Cost', headerValues]
 
-period_SPI = period_BCWP / period_BCWS
-period_SV = period_BCWP - period_BCWS
+#get CAM names
+cam_names = data_file['CAM'].drop_duplicates().tolist()
 
-period_CPI = period_BCWP / period_ACWP
-period_CV = period_BCWP - period_ACWP
+cams = {}
 
-period_DataFrame.loc['Period SV'] = period_SV
-period_DataFrame.loc['Period SPI'] = period_SPI
+for cams in cam_names:
+    cams[cam_names] = pd.DataFrame()
 
-period_DataFrame.loc['Period CV'] = period_CV
-period_DataFrame.loc['Period CPI'] = period_CPI
-
-curentMonth_ACWP = period_DataFrame.loc['Period Total Cost', headerValues[-1]]
-
-# Cumultive Data
-cum_DataFrame = period_DataFrame
-
-cum_DataFrame.loc['Cumulative Total Cost'] = cum_DataFrame.loc['Period Total Cost'].cumsum()
-cum_DataFrame.loc['Cumulative Planned Value'] = cum_DataFrame.loc['Period Total Planned'].cumsum()
-cum_DataFrame.loc['Cumulative Earned Value'] = cum_DataFrame.loc['Period Total Earned'].cumsum()
-
-cum_BCWP = cum_DataFrame.loc['Cumulative Earned Value', headerValues]
-cum_BCWS = cum_DataFrame.loc['Cumulative Planned Value', headerValues]
-cum_ACWP = cum_DataFrame.loc['Cumulative Total Cost', headerValues]
-
-cum_CV = cum_BCWP - cum_ACWP
-cum_CPI = cum_BCWP / cum_ACWP
-
-cum_SV = cum_BCWP - cum_BCWS
-cum_SPI = cum_BCWP / cum_BCWS
-
-cum_DataFrame.loc['SPI'] = cum_SPI
-cum_DataFrame.loc['SV'] = cum_SV
-
-cum_DataFrame.loc['CPI'] = cum_CPI
-cum_DataFrame.loc['CV'] = cum_CV
-
-# bcwr
-bac = cum_DataFrame['Total Cost'].sum()
-bcwp =cum_DataFrame.loc['Period Total Earned', 'Total Earned']
-percent_complete = bcwp / bac                  
-bcwr = bac - bcwp                                
-
-cum_todateUI_table = {"Budget at Complete": bac, "BCWP": bcwp, "Percent Complete": percent_complete,
-                      "Bugeted Cost of Work Remaining": bcwr}
-
-# eac
-acwp = cum_DataFrame.loc['Period Total Cost', 'Total Actual Cost']
-bcws = cum_DataFrame.loc['Period Total Planned', 'Total Planned']
-
-cum_todateUI_table["BCWS"] = bcws
-cum_todateUI_table["ACWP"] = acwp
-
-eac = acwp + bcwr                           # Estimate at complete
-tcpi = bac / eac                            # To Complete Perforamce Index
-
-cum_todateUI_table["EAC"] = eac
-cum_todateUI_table["TCPI"] = tcpi
-
-project_CPI = bcwp / acwp
-project_SPI = bcwp / bcws
-project_CV = bcwp - acwp
-project_SV = bcwp - bcws
-
-cum_todateUI_table["CPI"] = project_CPI
-cum_todateUI_table["SPI"] = project_SPI
-cum_todateUI_table["CV"] = project_CV
-cum_todateUI_table["SV"] = project_SV
-
-performance_ETC = acwp + (bcwr * project_CPI)   # Ajusted EAC
-cum_todateUI_table["Performance ETC"] = performance_ETC
-
-performance_EAC = acwp + performance_ETC        # Performance EAC
-cum_todateUI_table["Performance EAC"] = performance_EAC
-
-performance_tpci = bac / performance_EAC
-cum_todateUI_table["Perfromance TCPI"] = performance_tpci
-
-varaince_at_complete = bac - eac
-cum_todateUI_table["VAC"] = varaince_at_complete
-
-
-    
 # make list of CAMs for data processing
-cam_group = data_file.groupby('CAM')
-cam_name = 0
-for cam, group in cam_group:
-    cam_name = cam_name + 1
-    print('Cam', cam_name, cam)
-    print(group)
+#cam_group = data_file.groupby('CAM')
+#cam_name = 0
+#for cam, group in cam_group:
+    #cam_name = cam_name + 1
+    #print('Cam', cam_name, cam)
+    #print(group)
